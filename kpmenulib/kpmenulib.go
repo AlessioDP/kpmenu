6187ba57a6a19eba993654bd83091d786b7c61a5
AlessioDP <alessiodptv@gmail.com>
2019-10-14 09:55:12 +0200

    Release 1.2.1

--- kpmenulib/kpmenulib.go
+++ kpmenulib/kpmenulib.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Version is the version of kpmenu
-const Version = "1.2.0"
+const Version = "1.2.1"
 
 // Initialize is the function that initialize a menu, handle config and parse cli arguments
 func Initialize() *Menu {
